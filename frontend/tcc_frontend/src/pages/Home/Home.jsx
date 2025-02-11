7194bd439813aa613ce1a4659bf36c702d0e9fcd
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-11 11:39:22 -0300

    Modificado para o Vite

--- frontend/tcc_frontend/src/pages/Home/Home.jsx
+++ frontend/tcc_frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-red-500">
       <h1>Bem-vindo à Página Inicial!</h1>
     </div>
   );
