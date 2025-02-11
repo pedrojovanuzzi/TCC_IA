368555a793e80a2b9a3fda3f5ed5cb446ab5d4c7
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-11 11:59:20 -0300

    Pagina Home Criada

--- frontend/tcc_frontend/src/pages/Home/Home.jsx
+++ frontend/tcc_frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+'use client'
 
-export default function Home() {
+import { useState } from 'react'
+import img from "../../assets/imgs/hero1.webp";
+import img2 from "../../assets/imgs/hero2.png";
+
+
+
+export default function Example() {
+
   return (
-    <div className="bg-red-500">
-      <h1>Bem-vindo à Página Inicial!</h1>
+    <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Faculdades Integradas de Bauru</span>
+              <img
+                alt=""
+                src={img2}
+                className="h-10 w-auto"
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+          </div>
+        </nav>
+      </header>
+      <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+        <div
+          aria-hidden="true"
+          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-8 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+            <h1 className="max-w-2xl text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto">
+              Visão Computacional em Real Time
+            </h1>
+            <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+              <p className="text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+                Utilize de nosso modelo treinado para verificar se seus colaboradores estão utilizando o EPI com a câmera
+              </p>
+              <div className="mt-10 flex items-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Começar a Usar
+                </a>
+                <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                  Ler Mais <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+            <img
+              alt=""
+              src={img}
+              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+            />
+          </div>
+        </div>
+        <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+      </div>
     </div>
-  );
+  )
 }
