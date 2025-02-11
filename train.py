dab03a7d5e5eb9db34281c4f8bb26ff72b2fba5c
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-11 15:29:20 -0300

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,26 @@
 from ultralytics import YOLO
 
+
+# TREINAMENTO DO ZERO
 # Carregar modelo base pré-treinado (Transfer Learning)
-model = YOLO("yolov8n.pt")
+# model = YOLO("yolov8n.pt")
+
+# if __name__ == '__main__':
+# # Treinar o modelo com o novo dataset
+#     model.train(
+#         data="dataset.yaml",
+#         epochs=30, 
+#         batch=16, 
+#         imgsz=512, 
+#         device=0,
+#         workers=4,  # Melhor para Windows
+#         cache="disk", # Usa cache no disco, mais estável
+#         patience=20  # Para se não houver melhoria por 20 epochs
+#     )
+
+
+#Treinamento Fine-Tuning
+model = YOLO("./runs/train4/weights/best.pt")
 
 if __name__ == '__main__':
 # Treinar o modelo com o novo dataset
@@ -15,5 +34,3 @@ if __name__ == '__main__':
         cache="disk", # Usa cache no disco, mais estável
         patience=20  # Para se não houver melhoria por 20 epochs
     )
-
-
