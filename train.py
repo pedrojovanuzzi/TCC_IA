c1d21b3075d41d5dccf827012d99c295e79775a0
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-13 15:54:17 -0300

    treino 8 medio

--- train.py
+++ train.py
@@ -10,12 +10,12 @@ model = YOLO("./yolo11m.pt")
 
 #Treinamento Fine-Tuning
 # Garante que o caminho do modelo seja absoluto
-script_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(script_dir, "runs", "detect", "train7", "weights", "best.pt")
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# model_path = os.path.join(script_dir, "runs", "detect", "train7", "weights", "best.pt")
 
-# Confirma se o arquivo realmente existe
-if not os.path.exists(model_path):
-    raise FileNotFoundError(f"Arquivo não encontrado: {model_path}")
+# # Confirma se o arquivo realmente existe
+# if not os.path.exists(model_path):
+#     raise FileNotFoundError(f"Arquivo não encontrado: {model_path}")
 
 # model.load(model_path)  # Usa pesos do YOLOv8n treinado
 
