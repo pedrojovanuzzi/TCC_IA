70c441ff3d8d588b132f9349d8a77099099154c1
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-14 00:00:59 -0300

    Outras modificações

--- inference_view.py
+++ inference_view.py
@@ -20,7 +20,7 @@ app.add_middleware(
 
 # Carrega o modelo YOLO treinado
 # modelo_yolo = YOLO("runs/detect/train7/weights/best.pt").to("cuda")
-modelo_yolo = YOLO("runs/detect/train8/weights/best.engine")
+modelo_yolo = YOLO("runs/detect/train9/weights/best.engine")
 
 
 @app.websocket("/ws")
