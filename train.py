68c2f2e03f6320e798d01a1cfdab8d5a2d54022f
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-15 10:18:42 -0300

    Treino 11 Realizado

--- train.py
+++ train.py
@@ -4,21 +4,21 @@ from sahi import AutoDetectionModel
 from sahi.predict import get_sliced_prediction
 
 # NANO yolov11n / MEDIUM yolov11m
-model_path = "./yolo11m.pt"
+# model_path = "./yolo11m.pt"
 
 # Caminho do modelo treinado
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# model_path = os.path.join(script_dir, "runs", "detect", "train10", "weights", "best.pt")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(script_dir, "runs", "detect", "train10", "weights", "best.pt")
 
 # # Confirma se o arquivo do modelo existe
-# if not os.path.exists(model_path):
-#     raise FileNotFoundError(f"Arquivo não encontrado: {model_path}")
+if not os.path.exists(model_path):
+    raise FileNotFoundError(f"Arquivo não encontrado: {model_path}")
 
 # Treinamento do modelo (se necessário)
 if __name__ == '__main__':
     YOLO(model_path).train(
         data="dataset.yaml",
-        epochs=300,
+        epochs=10,
         imgsz=640,
         batch=-1,
         optimizer='auto',
