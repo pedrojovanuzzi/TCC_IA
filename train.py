13293700efd13de1f76a127f9dc14deec6783270
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-20 14:35:18 -0300

    Adicionado Modelo 12

--- train.py
+++ train.py
@@ -3,16 +3,16 @@ import os
 from sahi import AutoDetectionModel
 from sahi.predict import get_sliced_prediction
 
-# NANO yolov11n / MEDIUM yolov11m
-# model_path = "./yolo11m.pt"
+# NANO yolov12n / MEDIUM yolov11m
+model_path = "./yolo12m.pt"
 
 # Caminho do modelo treinado
-script_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(script_dir, "runs", "detect", "train13", "weights", "best.pt")
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# model_path = os.path.join(script_dir, "runs", "detect", "train13", "weights", "best.pt")
 
-# # Confirma se o arquivo do modelo existe
-if not os.path.exists(model_path):
-    raise FileNotFoundError(f"Arquivo não encontrado: {model_path}")
+# # # Confirma se o arquivo do modelo existe
+# if not os.path.exists(model_path):
+#     raise FileNotFoundError(f"Arquivo não encontrado: {model_path}")
 
 # Treinamento do modelo (se necessário)
 if __name__ == '__main__':
