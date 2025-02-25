4e386714e6fcdf424179dad8194e8c89265f324a
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-02-25 11:45:14 -0300

    Update vite.config.js

--- frontend/tcc_frontend/vite.config.js
+++ frontend/tcc_frontend/vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
