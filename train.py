638a0c3dc88a2b1dd2e2938aa0d2dca66dcb4ede
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-06-06 11:57:36 -0300

    Update train.py

--- train.py
+++ train.py
@@ -7,40 +7,17 @@ model_path = "./yolo11s.pt"
 # Treinamento do modelo (se necessário)
 if __name__ == '__main__':
     YOLO(model_path).train(
-    data="dataset.yaml",
-    epochs=300,
-    imgsz=416,  # ⬆️ aumenta o input size para melhorar detecção de objetos pequenos
-    batch=16,   # pode deixar -1 se quiser automático, mas 16 costuma ser bom
-    optimizer='auto',
-    device=0,
-    half=True,
-    amp=True,
-
-    # 🔧 Aumenta a robustez com augmentações
-    degrees=5,
-    scale=0.5,
-    translate=0.2,
-    shear=2.0,
-    perspective=0.0005,
-    flipud=0.0,  # evitar virar de cabeça pra baixo
-    fliplr=0.5,
-    mosaic=1.0,
-    mixup=0.1,
-
-    # 🔬 Extra (se quiser forçar mais atenção em objetos pequenos)
-    workers=4,
-    close_mosaic=15,  # desativa mosaic nos últimos 15 epochs
-    patience=50,
-)
+        data="dataset.yaml",
+        epochs=300,
+        imgsz=416,
+        batch=-1,
+        optimizer='auto',
+        device=0,
+        half=True,
+        amp=True,
+    )
     
 
-    # YOLO(model_path).train(
-    #     data="dataset.yaml",
-    #     epochs=300,
-    #     imgsz=416,
-    #     batch=-1,
-    #     optimizer='auto',
-    #     device=0,
-    #     half=True,
-    #     amp=True,
-    # )
+
+
+
