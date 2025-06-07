73a5045d91cbace693c32a9615131d228ca6cce5
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-06-07 11:33:00 -0300

    Modificações

--- utils/exportmodel.py
+++ utils/exportmodel.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 from multiprocessing import freeze_support
 
 def export_model():
-    model = YOLO("../runs/detect/train2/weights/best.pt")
+    model = YOLO("../runs/detect/train46/weights/best.pt")
     
     # Exporta para formato TensorRT (.engine)
     model.export(format="engine", device="cuda")  # usa GPU para exportar
