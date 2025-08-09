1b66053cc6fe16e583355e835d686dd3e1b13d5a
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-08-09 17:04:52 -0300

    Treino 4

--- train.py
+++ train.py
@@ -2,14 +2,14 @@ from ultralytics import YOLO
 
 
 # NANO yolov12n / MEDIUM yolov11m
-model_path = "./yolo11s.pt"
+model_path = "./yolo12s.pt"
 
 # Treinamento do modelo (se necessário)
 if __name__ == '__main__':
     YOLO(model_path).train(
         data="data.yaml",
         epochs=300,
-        imgsz=416,
+        imgsz=640,
         batch=16,
         optimizer='auto',
         device=0,
