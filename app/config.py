142b46771c277c389d4dae6aa82e501bd20af47c
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-10-09 14:12:30 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -22,7 +22,7 @@ ENCRYPTION_KEY = key_bytes
 
 # Outras configurações
 IS_LOCAL = os.getenv("LOCAL") == "true"
-TRAIN    = os.getenv("TRAIN", "train5")
+TRAIN    = os.getenv("TRAIN", "train7")
 TRAIN_LONG_DISTANCE    = os.getenv("TRAIN", "train6")
 
 MONITORINGFRAMES = os.getenv("MONITORINGFRAMES")
